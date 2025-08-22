724e48f4137439fa82e3d728a0d949eb83dc3c0a
taib0101 <taibvai13@gmail.com>
2025-08-22 20:41:17 +0600

    chore: update urlpatterns

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from src.routers import v1
 
 
-urlpatterns = [ 
+urlpatterns = [
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),    
     path("api/", include(v1))
 ]
 
