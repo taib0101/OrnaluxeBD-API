d02f87d02a250536c051c57f6933c3d85a19b4b4
taib0101 <taibvai13@gmail.com>
2025-08-22 20:26:22 +0600

    chore: initial project setup

---
+++ backend/manage.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import uvicorn
+from django.core.management import execute_from_command_line
+
+def main() -> None:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+    execute_from_command_line(sys.argv)
+
+def run_uvicorn() -> None:
+    uvicorn.run(
+        "backend.asgi:application", 
+        host="127.0.0.1", 
+        port=8000, 
+        reload=True, 
+        log_level="info"
+    )
+
+
+if __name__ == '__main__':
+    if "runuvicorn" in sys.argv:
+        run_uvicorn()
+    else:
+        main()
