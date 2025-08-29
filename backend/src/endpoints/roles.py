6dad104cd6b0dfecfab9b45fa05c999231996625
taib0101 <taibvai13@gmail.com>
2025-08-30 01:00:35 +0600

    feat: delete extra endpoint

--- backend/src/endpoints/roles.py
+++ backend/src/endpoints/roles.py
@@ -3,7 +3,6 @@ from django.urls import path
 from src.views import RoleCreate, RoleRead, RoleReadAll, RoleUpdate, RoleDelete
 
 users_endpoint = [
-    # path("users/", UserViewSet.as_view(), name="users")
     path("roles/create", RoleCreate.as_view()),
     path("roles/update", RoleUpdate.as_view()),
     path("roles", RoleRead.as_view()),
