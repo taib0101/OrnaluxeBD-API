aa26f191f173ca5df14f0d045a82044e38097dfc
taib0101 <taibvai13@gmail.com>
2025-08-30 01:04:14 +0600

    feat: update roles_endpoint name

--- backend/src/endpoints/roles.py
+++ backend/src/endpoints/roles.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from src.views import RoleCreate, RoleRead, RoleReadAll, RoleUpdate, RoleDelete
 
-users_endpoint = [
+roles_endpoint = [
     path("roles/create", RoleCreate.as_view()),
     path("roles/update", RoleUpdate.as_view()),
     path("roles", RoleRead.as_view()),
