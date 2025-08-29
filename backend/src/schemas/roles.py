37b8362534c6cb35587226f01c6a5e2c789bbbca
taib0101 <taibvai13@gmail.com>
2025-08-30 00:50:34 +0600

    feat: add roles schemas

---
+++ backend/src/schemas/roles.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+
+class RoleBase(serializers.Serializer):
+    name = serializers.CharField()
+
+class RoleIn(RoleBase):
+    pass
+
+class RoleOut(RoleBase):
+    role_id = serializers.CharField()
+    created_at = serializers.DateTimeField()
+    updated_at = serializers.DateTimeField(required=False, allow_null=True)
+
+class RoleTotallOut(serializers.Serializer):
+    total = serializers.IntegerField()
+    data = RoleOut(many=True)
+
+
+class RoleUpdate(RoleIn):
+    pass
+
+class RoleDelete(serializers.Serializer):
+    message = serializers.CharField()
