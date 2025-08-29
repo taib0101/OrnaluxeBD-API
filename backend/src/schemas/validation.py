5ba435ad4f22152aeb7c90876b93fd5620677f27
taib0101 <taibvai13@gmail.com>
2025-08-30 00:51:11 +0600

    feat: add schema validation for input and output data

---
+++ backend/src/schemas/validation.py
@@ -0,0 +1,29 @@
+from rest_framework.exceptions import ValidationError
+
+from src.supports import AppExceptionCase, AppException
+
+def input_validation(SchemaName, data_in):
+    try:
+        data_in = SchemaName(data=data_in)
+        data_in.is_valid(raise_exception=True)
+        data_in = data_in.validated_data
+
+        return data_in
+
+    except ValidationError:
+        return AppExceptionCase.BadRequest("Bad Request")
+    
+
+def output_validation(SchemaName, data_out):
+    try:
+
+        if not isinstance(data_out, AppException):
+            data_out = SchemaName(instance=data_out) # or SchemaName(instance=data_out, many=True) it works like [{"data": "message"}]
+
+            return data_out.data
+    
+        return data_out
+    
+    except ValidationError:
+        return AppExceptionCase.DatabaseError("Database Error")
+
