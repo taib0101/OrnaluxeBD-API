1d23169e83d1c47faa8cf99f232061d0b6187397
taib0101 <taibvai13@gmail.com>
2025-09-02 18:59:18 +0600

    func: update handle_result

--- backend/src/supports/handlers.py
+++ backend/src/supports/handlers.py
@@ -3,10 +3,11 @@ from rest_framework.response import Response
 
 
 def handle_result(exception, context):
+    response = exception_handler(exception, context)
+
     print("exception : ", exception)
     print("context: ", context)
-
-    response = exception_handler(exception, context)
+    print("response: ", response)
 
     if response:
         return Response({"error": exception.message}, status=exception.status_code)
