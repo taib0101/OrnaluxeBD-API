06dcad84f799ea13b8db24a8887f56b56660eb7e
taib0101 <taibvai13@gmail.com>
2025-09-03 00:33:10 +0600

    feat: add auth views

--- backend/src/views/auths.py
+++ backend/src/views/auths.py
@@ -1,16 +1,9 @@
 from rest_framework.views import APIView
 
-<<<<<<< HEAD
-from src.schemas import AuthLogIn, AuthTokenOut
-from src.docs import login, auth_check
-from src.middleware import LoggedInAdmin
-from src.services import auth_services
-=======
 from src.schemas import AuthLogIn, AuthTokenOut, UserOut
 from src.middleware import LoggedInAdmin
 from src.services import auth_services
 from src.docs import login, auth_check
->>>>>>> master
 
 class Login(APIView):
 
@@ -22,27 +15,14 @@ class Login(APIView):
             data = auth_services.login(request=request)
 
         return data
-<<<<<<< HEAD
-    
-=======
-
->>>>>>> master
 
 class Auth(APIView):
 
     authentication_classes = [LoggedInAdmin]
 
-<<<<<<< HEAD
     @auth_check(request=None, responses=AuthTokenOut)
-=======
-    @auth_check(request=None, responses=UserOut)
->>>>>>> master
     def get(self, request):
         if request.method == "GET":
             data = auth_services.auth_check(request=request)
 
         return data
-<<<<<<< HEAD
-
-=======
->>>>>>> master
