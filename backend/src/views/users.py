1f04416eef04c94c29e85c403a75b4d778f98c8a
taib0101 <taibvai13@gmail.com>
2025-09-02 18:54:45 +0600

    feat: add users views

--- backend/src/views/users.py
+++ backend/src/views/users.py
@@ -1,25 +1,64 @@
-from drf_spectacular.utils import extend_schema
-from rest_framework import serializers
-from rest_framework.response import Response
 from rest_framework.views import APIView
 
-# Define schema using a serializer
-class GreetingSerializer(serializers.Serializer):
-    message = serializers.CharField()
+from src.schemas import UserIn, UserOut, UserTotalOut, UserUpdate, UserDelete
+from src.middleware import LoggedInAdmin
+from src.docs import create_user, read_user_query, read_user_all, delete_user
+from src.services import user_services
 
-class UserViewSet(APIView):
-    authentication_classes = []  
+class UserCreate(APIView):
 
-    @extend_schema(
-        auth=[{"jwtAuth": []}],
-        summary="Bro",
-        description="Simple greeting endpoint",
-        tags=["Shitt"],
-        responses=GreetingSerializer
-    )
-    def get(self, request) -> Response:
-        print("Request Authorization : ", request.headers['Authorization'])
-        context = {
-            "name": "Murtaza"
-        }
-        return Response(f"Hello, {context['name']}")
+    authentication_classes = [LoggedInAdmin]
+
+    @create_user(request=UserIn, responses=UserOut)
+    def post(self, request):
+        if request.method == "POST":
+            data = user_services.create_user(request=request)
+
+        return data
+    
+
+class UserRead(APIView):
+
+    authentication_classes = [LoggedInAdmin]
+
+    @read_user_query(request=None, responses=UserTotalOut)
+    def get(self, request):
+        if request.method == "GET":
+            data = user_services.read_user_query(request=request)
+
+        return data
+    
+class UserReadAll(APIView):
+
+    authentication_classes = [LoggedInAdmin]
+
+    @read_user_all(request=None, responses=UserTotalOut)
+    def get(self, request):
+        if request.method == "GET":
+            data = user_services.read_user_all(request=request)
+
+        return data
+
+class UserUpdate(APIView):
+
+    authentication_classes = [LoggedInAdmin]
+
+    @delete_user(request=UserUpdate, responses=UserTotalOut)
+    def post(self, request):
+        if request.method == "POST":
+            data = user_services.update_user(request=request)
+
+        return data
+    
+
+class UserDelete(APIView):
+
+    authentication_classes = [LoggedInAdmin]
+
+    @delete_user(request=None, responses=UserDelete)
+    def get(self, request):
+        if request.method == "GET":
+            data = user_services.delete_user(request=request)
+
+        return data
+    
