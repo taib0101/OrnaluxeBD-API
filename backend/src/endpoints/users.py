93f5ee9467a777fbee8e88de0c372a35d4f17c24
taib0101 <taibvai13@gmail.com>
2025-09-02 19:24:15 +0600

    feat: add endpoints

--- backend/src/endpoints/users.py
+++ backend/src/endpoints/users.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from src.views import UserViewSet
+from src.views import UserCreate, UserRead, UserReadAll, UserUpdate, UserDelete
 
 users_endpoint = [
-    path("users/", UserViewSet.as_view(), name="users")
+    path("users/create", UserCreate.as_view()),
+    path("users/update", UserUpdate.as_view()),
+    path("users", UserRead.as_view()),
+    path("users/all", UserReadAll.as_view()),
+    path("users/delete", UserDelete.as_view())
 ]
