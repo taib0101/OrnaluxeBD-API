6ba331b91324ca077bb57aa91fd58119bb4721f5
taib0101 <taibvai13@gmail.com>
2025-09-02 15:20:06 +0600

    feat: add APIViews

---
+++ backend/src/views/auths.py
@@ -0,0 +1,29 @@
+from rest_framework.views import APIView
+
+from src.schemas import AuthLogIn, AuthTokenOut, UserOut
+from src.middleware import LoggedInAdmin
+from src.services import auth_services
+from src.docs import login, auth_check
+
+class Login(APIView):
+
+    authentication_classes = []
+
+    @login(request=AuthLogIn, responses=AuthTokenOut)
+    def post(self, request):
+        if request.method == "POST":
+            data = auth_services.login(request=request)
+
+        return data
+
+
+class Auth(APIView):
+
+    authentication_classes = [LoggedInAdmin]
+
+    @auth_check(request=None, responses=UserOut)
+    def get(self, request):
+        if request.method == "GET":
+            data = auth_services.auth_check(request=request)
+
+        return data
