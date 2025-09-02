9e3ffd0daf88484c0659e6342222d44bee104b23
taib0101 <taibvai13@gmail.com>
2025-09-03 00:32:11 +0600

    func: add update uuid4

--- backend/src/utils/generate_id.py
+++ backend/src/utils/generate_id.py
@@ -3,9 +3,6 @@ import uuid
 def unique_id():
     unique_id = str(uuid.uuid4()).split("-")
     unique_id = "".join([value for value in unique_id])
-<<<<<<< HEAD
 
-=======
->>>>>>> master
     return unique_id[:20]
 
