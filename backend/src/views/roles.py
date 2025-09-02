a1439b52c54dc1386e5b24fd5ec2b065e476667c
taib0101 <taibvai13@gmail.com>
2025-09-02 18:54:12 +0600

    feat: add  middleware for every views and update schema name RoleTotalOut

--- backend/src/views/roles.py
+++ backend/src/views/roles.py
@@ -1,47 +1,50 @@
 from rest_framework.views import APIView
-from typing import List
 
 from src.docs import create_role, read_role_query, read_role_all, update_role, delete_role
-from src.schemas import RoleIn, RoleOut, RoleTotallOut, RoleUpdate, RoleDelete
+from src.middleware import LoggedInAdmin
+from src.schemas import RoleIn, RoleOut, RoleTotalOut, RoleUpdate, RoleDelete
 from src.services import role_services
 
 
 class RoleCreate(APIView):
 
-    authentication_classes = []
+    authentication_classes = [LoggedInAdmin]
 
     @create_role(request=RoleIn, responses=RoleOut)
     def post(self, request):
         if request.method == "POST":
             data = role_services.create_role(request=request)
+
         return data
     
 
 class RoleRead(APIView):
 
-    authentication_classes = []
+    authentication_classes = [LoggedInAdmin]
 
-    @read_role_query(request=None, responses=RoleTotallOut)
+    @read_role_query(request=None, responses=RoleTotalOut)
     def get(self, request):
         if request.method == "GET":
             data = role_services.read_role_query(request=request)
+
         return data
     
 class RoleReadAll(APIView):
 
-    authentication_classes = []
+    authentication_classes = [LoggedInAdmin]
 
-    @read_role_all(request=None, responses=RoleTotallOut)
+    @read_role_all(request=None, responses=RoleTotalOut)
     def get(self, request):
         if request.method == "GET":
             data = role_services.read_role_all(request=request)
+
         return data
 
 class RoleUpdate(APIView):
 
-    authentication_classes = []
+    authentication_classes = [LoggedInAdmin]
 
-    @update_role(request=RoleUpdate, responses=RoleTotallOut)
+    @update_role(request=RoleUpdate, responses=RoleTotalOut)
     def post(self, request):
         if request.method == "POST":
             data = role_services.update_role(request=request)
@@ -51,7 +54,7 @@ class RoleUpdate(APIView):
 
 class RoleDelete(APIView):
 
-    authentication_classes = []
+    authentication_classes = [LoggedInAdmin]
 
     @delete_role(request=None, responses=RoleDelete)
     def get(self, request):
