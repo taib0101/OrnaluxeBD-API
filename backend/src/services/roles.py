e7a62afabfa2b668e1519967506a80d1c551d87f
taib0101 <taibvai13@gmail.com>
2025-09-02 19:10:59 +0600

    feat: update RoleTotalOut schema

--- backend/src/services/roles.py
+++ backend/src/services/roles.py
@@ -2,8 +2,7 @@ from rest_framework.response import Response
 from json import loads
 
 from src.repositories import role_repos
-from src.schemas import RoleIn, RoleOut, RoleTotallOut, RoleUpdate, input_validation, output_validation
-from src.supports import AppException, handle_result
+from src.schemas import RoleIn, RoleOut, RoleTotalOut, RoleUpdate, input_validation, output_validation
 
 class RoleService:
 
@@ -14,38 +13,24 @@ class RoleService:
         requested_body = loads(request.body)
 
         data_in = input_validation(SchemaName=RoleIn, data_in=requested_body)
-        if isinstance(data_in, AppException):
-            return handle_result(exception=data_in, context="")
 
         data = self.repo.create(ModelName="Role", data_in=data_in, temp_write="no")
-
         data = output_validation(SchemaName=RoleOut, data_out=data)
-        if isinstance(data, AppException):
-            return handle_result(exception=data, context="")
 
         return Response(data, status=200)
     
     def read_role_all(self, request):
-        query_data = request.GET.dict()
-
         data = self.repo.read_all(ModelName="Role")
-
-        data = output_validation(SchemaName=RoleTotallOut, data_out=data)
-        if isinstance(data, AppException):
-            return handle_result(exception=data, context="")
+        data = output_validation(SchemaName=RoleTotalOut, data_out=data)
 
         return Response(data, status=200)
     
     def read_role_query(self, request):
         query_data = request.GET.dict()
-
         query_data["role_id"] = query_data.pop("id")
 
         data = self.repo.read(ModelName="Role", query_data=query_data)
-
-        data = output_validation(SchemaName=RoleTotallOut, data_out=data)
-        if isinstance(data, AppException):
-            return handle_result(exception=data, context="")
+        data = output_validation(SchemaName=RoleTotalOut, data_out=data)
 
         return Response(data, status=200)
     
@@ -53,17 +38,12 @@ class RoleService:
         requested_body = loads(request.body)
         
         data_update = input_validation(SchemaName=RoleUpdate, data_in=requested_body)
-        if isinstance(data_update, AppException):
-            return handle_result(exception=data_update, context="")
 
         query_data = request.GET.dict()
         query_data["role_id"] = query_data.pop("id")
 
         data = self.repo.update(ModelName="Role", query_data=query_data, data_update=data_update, temp_write="no")
-
-        data = output_validation(SchemaName=RoleTotallOut, data_out=data)
-        if isinstance(data, AppException):
-            return handle_result(exception=data, context="")
+        data = output_validation(SchemaName=RoleTotalOut, data_out=data)
 
         return Response(data, status=200)
     
@@ -75,4 +55,4 @@ class RoleService:
 
         return Response(data, status=200)
     
-role_services = RoleService(role_repos)
+role_services = RoleService(repo=role_repos)
