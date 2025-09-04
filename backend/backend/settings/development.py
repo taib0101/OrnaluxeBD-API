a18bf9243077c4089321ad66af9acb8aeeb92cc9
taib0101 <taibvai13@gmail.com>
2025-09-04 18:50:43 +0600

    feat: add development settings

---
+++ backend/backend/settings/development.py
@@ -0,0 +1,18 @@
+import dj_database_url
+import os
+from dotenv import load_dotenv
+
+from .base import *
+
+# load dotenv
+load_dotenv(os.path.join(os.path.dirname(__file__), "../../.env"))
+
+SECRET_TOKEN_KEY = os.getenv("DEVELOPMENT_SECRET_TOKEN_KEY")
+SECRET_TOKEN_ALGO = os.getenv("DEVELOPMENT_SECRET_TOKEN_ALGO")
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv("DEVELOPMENT_DATABASE_URL"), # local host: postgres://root:1234@localhost:5432/mydatabase
+        conn_max_age=None # keeps single connection for all quires + transactions session
+    )
+}
