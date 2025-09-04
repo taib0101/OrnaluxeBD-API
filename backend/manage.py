635678dd793b2c87331d858660b55ba70340eacf
taib0101 <taibvai13@gmail.com>
2025-09-04 18:48:07 +0600

    fix: update the DJANGO_SETTINGS_MODULE for migrate and runserver of development and production

--- backend/manage.py
+++ backend/manage.py
@@ -3,17 +3,12 @@
     Designation: Software Engineer
 """
 
-
 import os
 import sys
 import uvicorn
 from django.core.management import execute_from_command_line
 
-def main() -> None:
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-    execute_from_command_line(sys.argv)
-
-def run_uvicorn() -> None:
+def run_uvicorn():
     uvicorn.run(
         "backend.asgi:application", 
         host="127.0.0.1", 
@@ -22,9 +17,25 @@ def run_uvicorn() -> None:
         log_level="info"
     )
 
+def main():
+    sys_argv_list = sys.argv.copy()
+
+    if "runuvicorn" in sys.argv:
+        sys.argv.remove("runuvicorn")
+
+    if "production" in sys.argv:
+        settings_module = "backend.settings.production"
+        sys.argv.remove("production")
+    else:
+        settings_module = "backend.settings.development"
+        sys.argv.remove("development")
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
+    execute_from_command_line(sys.argv)
+
+    if "runuvicorn" in sys_argv_list:
+        run_uvicorn()
+
 
 if __name__ == '__main__':
-    if "runuvicorn" in sys.argv:
-        run_uvicorn()
-    else:
-        main()
+    main()
