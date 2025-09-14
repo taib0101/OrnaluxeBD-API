27ce4753129772f8d201056cfaee408435857d70
taib0101 <taibvai13@gmail.com>
2025-09-15 01:26:33 +0600

    feat: update UserOut

--- backend/src/schemas/users.py
+++ backend/src/schemas/users.py
@@ -13,13 +13,17 @@ class UserOut(UserBase):
     user_id = serializers.UUIDField()
     role_id = serializers.UUIDField(required=False, allow_null=True)
     role_name = serializers.CharField(required=False, allow_null=True)
+    created_at = serializers.DateTimeField()
+    updated_at = serializers.DateTimeField(required=False, allow_null=True)
 
 class UserTotalOut(serializers.Serializer):
     total = serializers.IntegerField()
     data = UserOut(many=True)
 
-class UserUpdate(UserBase):
-    pass
+class UserUpdate(serializers.Serializer):
+    name = serializers.CharField(required=False, allow_null=True)
+    email = serializers.EmailField(required=False, allow_null=True)
+    phone = serializers.CharField(required=False, allow_null=True)
 
 class UserDelete(serializers.Serializer):
     message = serializers.CharField()
