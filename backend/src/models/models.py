a87a8913de3c3a4a88a7565c75d9572c384cf4dc
taib0101 <taibvai13@gmail.com>
2025-09-15 01:33:41 +0600

    feat: update attribute serial_number in Role, User

--- backend/src/models/models.py
+++ backend/src/models/models.py
@@ -4,18 +4,26 @@ from src.utils import unique_id
 
 class Role(models.Model):
 
+    serial_number = models.IntegerField(unique=True, null=False)
     role_id = models.CharField(primary_key=True, default=unique_id, unique=True)
     name = models.CharField(max_length=100, unique=True, null=False)
     created_at = models.DateTimeField(null=False)
     updated_at = models.DateTimeField(null=True)
-
+    
     class Meta:
         db_table = "roles"
         models.Index(fields=["role_id"])
 
+    # override for autoincrement
+    def save(self, *args, **kwargs):
+        last = Role.objects.order_by("-serial_number").first()
+        self.serial_number = (last.serial_number + 1) if last else 1
+        super().save(*args, **kwargs)
+
 
 class User(models.Model):
 
+    serial_number = models.IntegerField(unique=True, null=False)
     user_id = models.CharField(primary_key=True, default=unique_id, unique=True)
 
     # One to One                   # on delete cascade                  # back_populates like sqlalchemy
@@ -33,4 +41,9 @@ class User(models.Model):
         models.Index(fields=["user_id"])
         models.Index(fields=["email"])
         models.Index(fields=["phone"])
+
+    def save(self, *args, **kwargs):
+        last = User.objects.order_by("-serial_number").first()
+        self.serial_number = (last.serial_number + 1) if last else 1
+        super().save(*args, **kwargs)
     
