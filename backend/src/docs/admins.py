eb3553bf4aac918191a9a613f498596f855abc81
taib0101 <taibvai13@gmail.com>
2025-09-15 01:35:50 +0600

    feat: update sign_up summary

--- backend/src/docs/admins.py
+++ backend/src/docs/admins.py
@@ -4,7 +4,7 @@ from drf_spectacular.utils import extend_schema
 def sign_up(request, responses):
 
     return extend_schema(
-        summary="SignUp",
+        summary="SignUp Admin Initially",
         description="SignUp Admin Users",
         tags=["Admin"],
         request=request,
