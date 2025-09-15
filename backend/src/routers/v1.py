9a6f40346f46580187cfd34b688bccbf42e3e91c
taib0101 <taibvai13@gmail.com>
2025-09-15 19:46:34 +0600

    feat: update routers

--- backend/src/routers/v1.py
+++ backend/src/routers/v1.py
@@ -1,10 +1,17 @@
 from django.urls import path, include
 
-from src.endpoints import roles_endpoint, users_endpoint, auths_endpoint, admins_endpoint
+from src.endpoints import (
+    roles_endpoint, 
+    users_endpoint, 
+    auths_endpoint, 
+    admins_endpoint, 
+    categories_endpoint
+)
 
 v1 = [
     path("v1/", include(roles_endpoint)),
     path("v1/", include(users_endpoint)),
     path("v1/", include(auths_endpoint)),
-    path("v1/", include(admins_endpoint))
+    path("v1/", include(admins_endpoint)),
+    path("v1/", include(categories_endpoint))
 ]
