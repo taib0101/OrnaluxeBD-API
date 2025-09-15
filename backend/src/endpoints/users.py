1aea009dc6d431724208a13044901e1d005c9692
taib0101 <taibvai13@gmail.com>
2025-09-15 19:50:36 +0600

    feat: update user endpoints

--- backend/src/endpoints/users.py
+++ backend/src/endpoints/users.py
@@ -3,9 +3,9 @@ from django.urls import path
 from src.views import UserCreate, UserRead, UserReadAll, UserUpdate, UserDelete
 
 users_endpoint = [
-    path("users/create", UserCreate.as_view()),
-    path("users/update", UserUpdate.as_view()),
-    path("users", UserRead.as_view()),
-    path("users/all", UserReadAll.as_view()),
-    path("users/delete", UserDelete.as_view())
+    path("user/create", UserCreate.as_view()),
+    path("user/update", UserUpdate.as_view()),
+    path("user", UserRead.as_view()),
+    path("user/all", UserReadAll.as_view()),
+    path("user/delete", UserDelete.as_view())
 ]
