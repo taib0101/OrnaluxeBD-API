d8c7e88ff8b2ff808d034f29aaeedf88396b8b2e
taib0101 <taibvai13@gmail.com>
2025-09-19 17:59:19 +0600

    feat: update login method of AuthService

--- backend/src/services/categories.py
+++ backend/src/services/categories.py
@@ -1,20 +1,18 @@
 from rest_framework.response import Response
 from json import loads
 
-from src.repositories import category_repos
+from src.repositories import category_repos, CategoryRepo
 from src.schemas import CategoryIn, CategoryUpdate, CategoryOut, CategoryTotalOut, input_validation, output_validation
 
 class CategoryService:
 
-    def __init__(self, repo):
+    def __init__(self, repo: CategoryRepo):
         self.repo = repo
 
     def category_create(self, request):
         requested_body = loads(request.body)
 
         data_in = input_validation(SchemaName=CategoryIn, data_in=requested_body)
-
-        print("data_in : ", data_in)
 
         data = self.repo.create(ModelName="Category", data_in=data_in, temp_write="no")
         data = output_validation(SchemaName=CategoryOut, data_out=data)
@@ -30,7 +28,7 @@ class CategoryService:
     def read_category_query(self, request):
         query_data = request.GET.dict()
 
-        data = self.repo.read(ModelName="Category", query_data=query_data)
+        data = self.repo.read_category_query(query_data=query_data)
         data = output_validation(SchemaName=CategoryTotalOut, data_out=data)
 
         return Response(data, status=200)
