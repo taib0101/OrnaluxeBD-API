4b6a3be75b8a4d5c9db49981afd83fe28dca225e
taib0101 <taibvai13@gmail.com>
2025-09-19 18:11:46 +0600

    feat: add read_category_query method of CategoryRepo

--- backend/src/repositories/categories.py
+++ backend/src/repositories/categories.py
@@ -1,10 +1,17 @@
-from .base import *
-
+from .base import Base
 from src.models import dict_model, DictModel
-
+from src.supports import AppException, AppExceptionCase
 class CategoryRepo(Base):
 
     def __init__(self, model_dict: DictModel):
         super().__init__(model_dict=dict_model)
+
+    def read_category_query(self, query_data: dict):
+        data = self.read(ModelName="Category", query_data=query_data)
+
+        if isinstance(data, AppException):
+            raise AppExceptionCase.NotFoundError("Not Found")
+        
+        return data
 
 category_repos = CategoryRepo(model_dict=dict_model)
