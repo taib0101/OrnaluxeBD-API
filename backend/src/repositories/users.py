04bc4933a4400042eb1b9a960cbcb6e7d0d02fd5
taib0101 <taibvai13@gmail.com>
2025-09-19 18:13:02 +0600

    feat: add read_user_query method of UserRepo

--- backend/src/repositories/users.py
+++ backend/src/repositories/users.py
@@ -1,10 +1,19 @@
 from .base import Base
 from src.models import dict_model, DictModel
-
+from src.supports import AppException, AppExceptionCase
 class UserRepo(Base):
 
     def __init__(self, model_dict: DictModel):
         super().__init__(model_dict=dict_model)
+
+    def read_user_query(self, query_data: dict):
+        data = self.read(ModelName="User", query_data=query_data)
+
+        if isinstance(data, AppException):
+            raise AppExceptionCase.NotFoundError("Not Found")
+        
+        return data
+
 
 
 user_repos = UserRepo(model_dict=dict_model)
