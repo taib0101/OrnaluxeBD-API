512e32e6306eacd284808df205631259be2ec2be
taib0101 <taibvai13@gmail.com>
2025-09-19 18:08:17 +0600

    feat: add login method of AuthRepo

--- backend/src/repositories/auths.py
+++ backend/src/repositories/auths.py
@@ -1,10 +1,28 @@
 from .base import Base
 from src.models import dict_model, DictModel
+from src.utils import Hash
+from src.supports import AppException, AppExceptionCase
 
 class AuthRepo(Base):
 
     def __init__(self, model_dict: DictModel):
-        super().__init__(model_dict)
+        super().__init__(model_dict=model_dict)
+        
+    def read_for_auth(self, query_data: dict):
+        temp_data_in = query_data.copy()
+        temp_data_in.pop('password')
+
+        login_data = self.read(ModelName="User", query_data=temp_data_in)
+
+        if isinstance(login_data, AppException):
+            raise AppExceptionCase.UnAuthorized("Invalid Login")
+
+        check_password = Hash.check_pass(query_data['password'], login_data['data'][0]['password'])
+
+        if not check_password:
+            raise AppExceptionCase.UnAuthorized("Invalid Login")
+        
+        return login_data
 
 
 auth_repos = AuthRepo(model_dict=dict_model)
