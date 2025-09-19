04da5c49ff0f6a9f1ec28b2b35bc4da9560b7fb7
taib0101 <taibvai13@gmail.com>
2025-09-19 18:12:21 +0600

    feat: add read_role_query method of RoleRepo

--- backend/src/repositories/roles.py
+++ backend/src/repositories/roles.py
@@ -1,11 +1,20 @@
 from .base import Base
-from src.models import Role
 from src.models import dict_model
+from src.supports import AppException, AppExceptionCase
 
 class RoleRepo(Base):
 
     def __init__(self, model_dict):
         super().__init__(model_dict)
+
+    def read_role_query(self, query_data: dict):
+        data = self.read(ModelName="Role", query_data=query_data)
+
+        if isinstance(data, AppException):
+            raise AppExceptionCase.NotFoundError("Not Found")
+        
+        return data
+
 
 
 role_repos = RoleRepo(model_dict=dict_model)
