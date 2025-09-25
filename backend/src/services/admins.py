5a5a92d77581abdd814825598f1d68a7081c5c8f
taib0101 <taibvai13@gmail.com>
2025-09-26 02:43:12 +0600

    feat: update admin services

--- backend/src/services/admins.py
+++ backend/src/services/admins.py
@@ -21,13 +21,10 @@ class AdminService:
 
         # role create of admin
         role_data = user_data = {}
-        role_data['name'] = admin_data.pop('role_name')
         role_db_data = self.repo.create(ModelName="Role", data_in=role_data, temp_write="no")
 
         # user create of admin
         user_data['role_id'] = role_db_data['role_id']
-        user_data['role_name'] = role_db_data['name']
-        user_data['name'] = admin_data['user_name']
         user_data['email'] = admin_data['email']
         user_data['phone'] = admin_data['phone']
         user_data['password'] = Hash.create_pass(admin_data['password'])
