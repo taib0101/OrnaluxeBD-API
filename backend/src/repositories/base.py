1fc9b6e736c34dca12f28e6f0a67e6642177005c
taib0101 <taibvai13@gmail.com>
2025-09-26 02:51:50 +0600

    feat: update base repositories, add database_error_info

--- backend/src/repositories/base.py
+++ backend/src/repositories/base.py
@@ -1,10 +1,21 @@
 from django.forms.models import model_to_dict
 from django.db import transaction, IntegrityError
-from django.core.exceptions import ValidationError
 from django.utils import timezone
 
 from src.supports import AppExceptionCase
 from src.models import DictModel
+
+def database_error_info(pg_error_code):
+    print("pg code error : ", (pg_error_code))
+
+    if pg_error_code == "23505":
+        return AppExceptionCase.Conflict("Duplicate Value")
+    elif pg_error_code == "23503":
+        return AppExceptionCase.BadRequest("Bad Request for Foreign key violation")
+    elif pg_error_code == "23502":
+        return AppExceptionCase.BadRequest("Bad Request for Not Null violation")
+    
+    return AppExceptionCase.BadRequest("Bad Request for Database")
 
 class Base:
     def __init__(self, model_dict: DictModel):
@@ -21,9 +32,12 @@ class Base:
                 
             return model_to_dict(create_data)
         
-        except IntegrityError:
-            raise AppExceptionCase.Conflict("Duplicate Value")
+        except IntegrityError as integrity_exception:
+            raise database_error_info(
+                pg_error_code=integrity_exception.__cause__.pgcode
+            )
     
+
     def read_all(self, ModelName: str):
         with transaction.atomic():
             read_data = self.model_dict[ModelName].objects.all()
@@ -33,21 +47,20 @@ class Base:
             "data": read_data
         }
     
+
     def read(self, ModelName: str, query_data: dict):
-        try:
-            with transaction.atomic():
-                read_data = self.model_dict[ModelName].objects.filter(**query_data)
-                if not read_data:
-                    raise AppExceptionCase.NotFoundError("Not Found")
+        with transaction.atomic():
+            read_data = self.model_dict[ModelName].objects.filter(**query_data)
 
-            return {
-                "total": len(read_data),
-                "data": list(read_data.values())
-            }
+            if not read_data:
+                return AppExceptionCase.NotFoundError("Not Found")
 
-        except ValidationError:
-            return AppExceptionCase.NotFoundError("Not Found")
+        return {
+            "total": len(read_data),
+            "data": list(read_data.values())
+        }
 
+        
     def update(self, ModelName: str, query_data: dict, data_update: dict, temp_write: str):
         try:
             with transaction.atomic():
@@ -64,23 +77,23 @@ class Base:
 
             return updated_data
         
-        except ValidationError:
-            raise AppExceptionCase.NotFoundError("Not Found")
+        except IntegrityError as integrity_exception:
+            raise database_error_info(
+                pg_error_code=integrity_exception.__cause__.pgcode
+            )
         
+
     def delete(self, ModelName: str, query_data: dict, temp_write: str):
-        try:
-            with transaction.atomic():
-                delete_data = self.model_dict[ModelName].objects.filter(**query_data).delete()
-                if not delete_data[0]:
-                    raise AppExceptionCase.NotFoundError("Not Found")
+        with transaction.atomic():
+            delete_data = self.model_dict[ModelName].objects.filter(**query_data).delete()
 
-                if temp_write == "yes":
-                    transaction.set_rollback(True)
-                    return None
+            if not delete_data[0]:
+                raise AppExceptionCase.NotFoundError("Not Found")
 
-            return {
-                "message": f"{delete_data[0]} data deleted"
-            }
+            if temp_write == "yes":
+                transaction.set_rollback(True)
+                return None
 
-        except ValidationError:
-            raise AppExceptionCase.NotFoundError("Not Found")
+        return {
+            "message": f"{delete_data[0]} data deleted"
+        }
