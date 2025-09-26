4f59813dc12a8b400abf8f8a9e763aed84ef611f
taib0101 <taibvai13@gmail.com>
2025-09-27 01:42:02 +0600

    feat: update admins repositories

--- backend/src/repositories/admins.py
+++ backend/src/repositories/admins.py
@@ -4,7 +4,7 @@ from src.models import dict_model, DictModel
 class AdminRepo(Base):
 
     def __init__(self, model_dict: DictModel):
-        super().__init__(model_dict=dict_model)
+        super().__init__(model_dict=model_dict)
 
 
 admin_repos = AdminRepo(model_dict=dict_model)
