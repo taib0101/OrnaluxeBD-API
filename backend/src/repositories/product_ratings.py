3fdb1701e1e999cb8087d3f6ac72dbd71e474acc
taib0101 <taibvai13@gmail.com>
2025-09-29 17:24:28 +0600

    feat: update product_images repositories

---
+++ backend/src/repositories/product_ratings.py
@@ -0,0 +1,20 @@
+from .base import Base
+from src.models import dict_model, DictModel
+from src.supports import AppException, AppExceptionCase
+
+
+class ProductRatingRepo(Base):
+
+    def __init__(self, model_dict: DictModel):
+        super().__init__(model_dict=model_dict)
+    
+
+    def read_product_rating_query(self, query_data: dict):
+        data = self.read(ModelName="Rating", query_data=query_data)
+
+        if isinstance(data, AppException):
+            raise AppExceptionCase.NotFoundError("Not Found")
+        
+        return data
+
+product_rating_repos = ProductRatingRepo(model_dict=dict_model)
