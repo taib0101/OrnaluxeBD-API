219c0cd9ea6cddc4cd18d8fab72a2949db9850bc
taib0101 <taibvai13@gmail.com>
2026-01-06 12:42:35 +0600

    feat: update products repo

--- backend/src/repositories/products.py
+++ backend/src/repositories/products.py
@@ -1,18 +1,15 @@
 from .base import Base
-from src.models import dict_model, DictModel
+from src.models import Model, Models
 from src.supports import AppException, AppExceptionCase
 
 class ProductRepo(Base):
-
-    def __init__(self, model_dict: DictModel):
-        super().__init__(model_dict=model_dict)
 
     def read_product_query(self, query_data: dict):
         data = self.read(ModelName="Product", query_data=query_data)
 
         if isinstance(data, AppException):
-            raise AppExceptionCase.NotFoundError("Not Found")
+            raise data
         
         return data
 
-product_repos = ProductRepo(model_dict=dict_model)
+product_repos = ProductRepo()
