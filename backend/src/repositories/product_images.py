846687dfd84351e81ab67b14097328209aff7702
taib0101 <taibvai13@gmail.com>
2026-01-06 12:41:59 +0600

    feat: update product_images repo

--- backend/src/repositories/product_images.py
+++ backend/src/repositories/product_images.py
@@ -1,19 +1,15 @@
 from .base import Base
-from src.models import dict_model, DictModel
+from src.models import Model, Models
 from src.supports import AppException, AppExceptionCase
 
 class ProductImageRepo(Base):
-
-    def __init__(self, model_dict: DictModel):
-        super().__init__(model_dict=model_dict)
-
 
     def read_product_image_query(self, query_data: dict):
         data = self.read(ModelName="ProductImage", query_data=query_data)
 
         if isinstance(data, AppException):
-            raise AppExceptionCase.NotFoundError("Not Found")
+            raise data
         
         return data
 
-product_image_repos = ProductImageRepo(model_dict=dict_model)
+product_image_repos = ProductImageRepo()
