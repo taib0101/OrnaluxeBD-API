18a3a2385fe3e3f6d30e98b9868233674a7a3cfd
taib0101 <taibvai13@gmail.com>
2026-01-06 17:06:45 +0600

    feat: update validation

--- backend/src/schemas/validation.py
+++ backend/src/schemas/validation.py
@@ -1,25 +1,18 @@
 from rest_framework.exceptions import ValidationError
 
-from src.supports import AppExceptionCase, AppException
+from src.supports import AppExceptionCase
 
 def input_validation(SchemaName, data_in):
-    try:
-        data_in = SchemaName(data=data_in)
-        data_in.is_valid(raise_exception=True)
-        data_in = data_in.validated_data
+    data_in = SchemaName(data=data_in)
+    data_in.is_valid(raise_exception=True)
+    data_in = data_in.validated_data
 
-        return data_in
-
-    except ValidationError:
-        raise AppExceptionCase.BadRequest("Bad Request")
+    return data_in
     
 
 def output_validation(SchemaName, data_out):
-    try:
         data_out = SchemaName(instance=data_out) # or SchemaName(instance=data_out, many=True) it works like [{"data": "message"}]
 
         return data_out.data
     
-    except ValidationError:
-        raise AppExceptionCase.DatabaseError("Database Error")
 
