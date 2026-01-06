f11993f61826a598e260e0fe84e8b8f1fd6b9a28
taib0101 <taibvai13@gmail.com>
2026-01-06 12:28:23 +0600

    feat: add custom_exception and update handle_result

--- backend/src/supports/handlers.py
+++ backend/src/supports/handlers.py
@@ -1,15 +1,35 @@
 from rest_framework.views import exception_handler
 from rest_framework.response import Response
+from django.db import IntegrityError
+from django.core.exceptions import FieldError
+
+from .exceptions import AppExceptionCase, AppException
+
+def custom_exception(exception, context):
+    print("exception : ", exception)
+    print("context: ", context)
+
+    response = None
+
+    if isinstance(exception, IntegrityError):
+        response = exception_handler(AppExceptionCase.Conflict("Duplicate Value"), context)
+
+    elif isinstance(exception, FieldError):
+        response = exception_handler(AppExceptionCase.BadRequest("Bad Request for Database"), context)
+
+    elif isinstance(exception, AppException):
+        response = exception_handler(exception, context)
+
+    print("response: ", response)
+
+    return response
 
 
 def handle_result(exception, context):
-    response = exception_handler(exception, context)
 
-    print("exception : ", exception)
-    print("context: ", context)
-    print("response: ", response)
+    response = custom_exception(exception=exception, context=context)
 
     if response:
-        return Response({"error": str(exception)}, status=response.status_code)
+        return Response({"error": response.data['detail']}, status=response.status_code)
 
     return Response({"error": str(exception)}, status=500)
