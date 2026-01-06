8a0003d9549d6f6fc465f2ed2920b6c7c165a5fe
taib0101 <taibvai13@gmail.com>
2026-01-06 12:43:03 +0600

    feat: update users repo

--- backend/src/repositories/users.py
+++ backend/src/repositories/users.py
@@ -1,19 +1,15 @@
 from .base import Base
-from src.models import dict_model, DictModel
+from src.models import Model, Models
 from src.supports import AppException, AppExceptionCase
 class UserRepo(Base):
-
-    def __init__(self, model_dict: DictModel):
-        super().__init__(model_dict=model_dict)
 
     def read_user_query(self, query_data: dict):
         data = self.read(ModelName="User", query_data=query_data)
 
         if isinstance(data, AppException):
-            raise AppExceptionCase.NotFoundError("Not Found")
+            raise data
         
         return data
 
 
-
-user_repos = UserRepo(model_dict=dict_model)
+user_repos = UserRepo()
